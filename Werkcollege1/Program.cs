9f68370cf1113fe0ffb8443ead09f02233ef6be1
nilswag <nilswagemaker05@icloud.com>
2026-04-22 17:32:26 +0200

    Added WC1 exercise 3

--- Werkcollege1/Program.cs
+++ Werkcollege1/Program.cs
@@ -2,12 +2,14 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Opdracht2();
+            Console.WriteLine('\n');
+            Opdracht3();
         }
 
-        static void Opdracht2()
+        private static void Opdracht2()
         {
             /*
             Invariant: Huizen[i] != Huizen[j]
@@ -42,6 +44,38 @@
             for (int i = startNr; i <= eindNr; i++) Console.Write($"{i} ");
             Console.Write('\n');
             foreach (char c in huizen) Console.Write($"{c} ");
+        }
+
+        private static void Opdracht3()
+        {
+            /*
+            Invariant: i <= portie < j
+            */
+            static int ZoekGrootstePortie(int[] pizzas, int n)
+            {
+                int i = 1; // minimale portie
+                int j = pizzas.Max();
+                int x = 0;
+
+                while (i < j - 1)
+                {
+                    x = (i + j) / 2;
+
+                    int nPorties = 0;
+                    foreach (int p in pizzas) nPorties += p / x;
+
+                    if (nPorties >= n)
+                        i = x;
+                    else
+                        j = x;
+                }
+
+                return x;
+            }
+
+            int[] pizzas = [7, 9];
+            int portie = ZoekGrootstePortie(pizzas, 3);
+            Console.WriteLine($"Grootste portie: {portie}");
         }
     }
 }
